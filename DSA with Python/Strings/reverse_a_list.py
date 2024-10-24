948f07d0d75d417ef309da219356cfa575981350
D Vamsidhar <vamsidhard2002@gmail.com>
2024-10-24 09:18:10 +0530

    Add files via upload

---
+++ DSA with Python/Strings/reverse_a_list.py
@@ -0,0 +1,8 @@
+# defining a list
+list1 = []
+
+# take the input from user regarding 
+list1 = str(input('Enter whatever string you want : '))
+
+print(f'The original string user entered: {list1}')
+print(f'The reversed string: {list1[::-1]}')
