9bdb851d0179e996a50b0117284f86ea3d976c71
D Vamsidhar <vamsidhard2002@gmail.com>
2024-10-26 14:33:31 +0530

    Add files via upload

---
+++ DSA with C++/Arrays/Reverse_an_array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> list1;  // Define a vector to store elements
+    int n;
+
+    // Take the input from user regarding number of elements
+    cout << "Enter number of elements you want in the array: ";
+    cin >> n;
+
+    // Loop to take elements from user
+    for (int i = 0; i < n; ++i) {
+        int num;
+        cout << "Enter element number " << i << ": ";
+        cin >> num;
+        list1.push_back(num);
+    }
+
+    // Display the original array
+    cout << "The original array user entered: ";
+    for (int i = 0; i < n; ++i) {
+        cout << list1[i] << " ";
+    }
+    cout << endl;
+
+    // Display the reversed array
+    cout << "The reversed array: ";
+    for (int i = n - 1; i >= 0; --i) {
+        cout << list1[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
